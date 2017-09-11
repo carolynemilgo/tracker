837c88a34d5159a77475d334d74dbb59f28747da
carolynemilgo <carolynemilgo@gmail.com>
2017-09-11 12:37:37 +0300

    background for templates

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: ./build/install/tracker/bin/tracker
--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,19 +7,19 @@ import java.util.List;
 import java.util.ArrayList;
 public class App{
   public static void main(String[] args){
-    // ProcessBuilder process = new ProcessBuilder();
-    //    Integer port;
-    //
-    //    // This tells our app that if Heroku sets a port for us, we need to use that port.
-    //    // Otherwise, if they do not, continue using port 4567.
-    //
-    //    if (process.environment().get("PORT") != null) {
-    //        port = Integer.parseInt(process.environment().get("PORT"));
-    //    } else {
-    //        port = 4567;
-    //    }
-    //
-    //    setPort(port);
+    ProcessBuilder process = new ProcessBuilder();
+       Integer port;
+
+       // This tells our app that if Heroku sets a port for us, we need to use that port.
+       // Otherwise, if they do not, continue using port 4567.
+
+       if (process.environment().get("PORT") != null) {
+           port = Integer.parseInt(process.environment().get("PORT"));
+       } else {
+           port = 4567;
+       }
+
+       setPort(port);
     staticFileLocation("/public");
     String layout = "templates/layout.vtl";
 
